f1da900a13e1bc2f7e7c2d46ff2591a0001519ef
Jag Singh <jag@infojam.co.uk>
2018-06-21 19:57:45 +0100

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+   <h2>Login.</h2>
+   <hr>
+    <input type="email" placeholder="Username" v-model="username"/>
+        <input type="password" v-model="password" />
+<button v-on:click="Submit()" />
+    
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    Submit: function() {
+      console.log(this.username + this.password);
+    }
+  },
+  mounted: function() {
+  
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
